117f7a7150c607b4a74ac715b90e9ce7e4acc8b1
Grace Abian <graceabian10@gmail.com>
2022-04-21 01:20:30 +0800

    preparing for deployment

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 export default axios.create({
-  //baseURL: "https://mighty-ravine-32012.herokuapp.com/",
-  baseURL: "http://localhost:8080/",
+  baseURL: "https://mighty-ravine-32012.herokuapp.com/",
+  //baseURL: "http://localhost:8080/",
   headers: {
     "Content-type": "application/json",
   },
